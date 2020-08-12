c59fe0dc85b754b3005bcf0076d220bd147300b9
Jason Dean <jdean22@gmail.com>
2020-08-12 17:24:18 -0400

    updated smtp config, changed port 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'heroku.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials[:sendgrid][:SENDGRID_USERNAME], 
+    password: Rails.application.credentials[:sendgrid][:SENDGRID_PASSWORD]
+  }
