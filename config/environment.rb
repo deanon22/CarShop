4459cf238290e19440394f4b3d14cd2d235380d7
Jason Dean <jdean22@gmail.com>
2020-08-12 17:33:34 -0400

    updated smtp config, changed port 3

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,3 @@ require_relative 'application'
 Rails.application.initialize!
 
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'heroku.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: Rails.application.credentials[:sendgrid][:SENDGRID_USERNAME], 
-    password: Rails.application.credentials[:sendgrid][:SENDGRID_PASSWORD]
-  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'carshop-rails.herokuapp.com', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'heroku.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials[:sendgrid][:SENDGRID_USERNAME], 
+    password: Rails.application.credentials[:sendgrid][:SENDGRID_PASSWORD]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
