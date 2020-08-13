efddd1a6a8b70c41076607c0880868d49d9e1181
Jason Dean <jdean22@gmail.com>
2020-08-13 15:44:36 -0400

    added rails credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,18 +65,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://carshop-rails.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'carshop-rails.herokuapp.com', protocol: 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'carshop-rails.herokuapp.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
-    }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
